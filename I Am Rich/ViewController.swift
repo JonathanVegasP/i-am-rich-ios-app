7956f65ec7b0415e802f938d57e02b2efb83a168
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2021-02-20 00:26:16 -0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jonathan Vegas Peixoto on 20/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
